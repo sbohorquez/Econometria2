1ad8a2d79aa986248567e76e807a3e7e12726c2f
sbohorquez <sbohorqu@eafit.edu.co>
2022-01-08 20:02:20 -0500

    Update scripts.js

--- docs/slides/js/scripts.js
+++ docs/slides/js/scripts.js
@@ -1,77 +1,29 @@
 var Slides = {
-   container : $('#slides'),
-
    totalSlides : '',
 
-   translateAmount : 0,
-
-   currentSlide : 0,
-
-   slideWidth : '',
-
-   init : function(totalSlides) {
-      var each;
-
-      if ( !totalSlides ) throw new Error('Please pass the total number of slides.');
+   init : function( totalSlides ) {
+      // If nothing was passed to this function, we can't continue. 
+      if ( !totalSlides ) throw new Error('Please pass the total number of slides to the init method');
       Slides.totalSlides = totalSlides;
 
+      // Load the slides
       Slides.loadContent();
-
-      each = Slides.container.children('div');
-      
-      // Determine the width of our canvas
-      Slides.slideWidth = each.width() + ( parseInt( each.css('margin-right'), 10 ) );
-
-      Slides.keyPress();
    },
 
-   loadContent : function() {
-      Slides.container.hide();
-      for ( var i = 0; i < Slides.totalSlides; i++ ) {
-         $('<div id="#slide-' + i + '"></div>')
-            .load('slides/' + i + '.html')
-            .appendTo(Slides.container);
-         }
-      Slides.container.show();
-   },
+  loadContent : function() {
+  // Hide the container. 
+   Slides.container.hide();
 
-   keyPress : function() {
-      $(document.body).keydown(function(e) {
-         // if left or right arrow key is pressed
-         if ( e.keyCode === 39 || e.keyCode === 37 ) {
-            e.preventDefault();
-            ( e.keyCode === 39 ) ? Slides.next() : Slides.prev();
-         }
-      });
-   },
+   for ( var i = 0; i < Slides.totalSlides; i++ ) {
+      $(''<div id="#slide-' + i + '">'</div>')
+         .load('slides/' + i + '.html')
+         .appendTo(Slides.container);
+      }
 
-   next : function( ) {
-      Slides.translateAmount -= Slides.slideWidth;
-      Slides.updateHash( ++Slides.currentSlide );
-      Slides.animate();
-   },
+   // Now display the slides container again - causing exactly one reflow.
+   Slides.container.show();
+}
+}
 
-  prev : function() {
-     // No more left to go back.
-      if ( Slides.translateAmount === 0 ) return;
-
-      Slides.translateAmount += Slides.slideWidth;
-      Slides.updateHash( --Slides.currentSlide );
-      Slides.animate();
-  },
-
-  animate : function() {
-     Slides
-      .container
-      .children()
-         .css( '-webkit-transform', 'translateX(' + Slides.translateAmount + 'px)' );
-  },
-
-  updateHash : function( direction ) {
-     // Update current Slides and hash.
-     location.hash = '#slide-' + Slides.currentSlide;
-  }
-};
-
-// All right; let's do this. 
-Slides.init(2);
+// All right; let's do this. We'll assume that we've created 6 slides, total.
+Slides.init( 6 );
