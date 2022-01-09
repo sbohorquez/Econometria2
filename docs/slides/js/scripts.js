7301a7310c50cb043f0112715cf6796fbf998313
sbohorquez <sbohorqu@eafit.edu.co>
2022-01-08 20:14:32 -0500

    Update scripts.js

--- docs/slides/js/scripts.js
+++ docs/slides/js/scripts.js
@@ -1,29 +1,77 @@
 var Slides = {
+   container : $('#slides'),
+
    totalSlides : '',
 
-   init : function( totalSlides ) {
-      // If nothing was passed to this function, we can't continue. 
-      if ( !totalSlides ) throw new Error('Please pass the total number of slides to the init method');
+   translateAmount : 0,
+
+   currentSlide : 0,
+
+   slideWidth : '',
+
+   init : function(totalSlides) {
+      var each;
+
+      if ( !totalSlides ) throw new Error('Please pass the total number of slides.');
       Slides.totalSlides = totalSlides;
 
-      // Load the slides
       Slides.loadContent();
+
+      each = Slides.container.children('div');
+      
+      // Determine the width of our canvas
+      Slides.slideWidth = each.width() + ( parseInt( each.css('margin-right'), 10 ) );
+
+      Slides.keyPress();
    },
 
-  loadContent : function() {
-  // Hide the container. 
-   Slides.container.hide();
+   loadContent : function() {
+      Slides.container.hide();
+      for ( var i = 0; i < Slides.totalSlides; i++ ) {
+         $('<div id="#slide-' + i + '"></div>')
+            .load('slides/' + i + '.html')
+            .appendTo(Slides.container);
+         }
+      Slides.container.show();
+   },
 
-   for ( var i = 0; i < Slides.totalSlides; i++ ) {
-      $(''<div id="#slide-' + i + '">'</div>')
-         .load('slides/' + i + '.html')
-         .appendTo(Slides.container);
-      }
+   keyPress : function() {
+      $(document.body).keydown(function(e) {
+         // if left or right arrow key is pressed
+         if ( e.keyCode === 39 || e.keyCode === 37 ) {
+            e.preventDefault();
+            ( e.keyCode === 39 ) ? Slides.next() : Slides.prev();
+         }
+      });
+   },
 
-   // Now display the slides container again - causing exactly one reflow.
-   Slides.container.show();
-}
-}
+   next : function( ) {
+      Slides.translateAmount -= Slides.slideWidth;
+      Slides.updateHash( ++Slides.currentSlide );
+      Slides.animate();
+   },
 
-// All right; let's do this. We'll assume that we've created 6 slides, total.
-Slides.init( 6 );
+  prev : function() {
+     // No more left to go back.
+      if ( Slides.translateAmount === 0 ) return;
+
+      Slides.translateAmount += Slides.slideWidth;
+      Slides.updateHash( --Slides.currentSlide );
+      Slides.animate();
+  },
+
+  animate : function() {
+     Slides
+      .container
+      .children()
+         .css( '-webkit-transform', 'translateX(' + Slides.translateAmount + 'px)' );
+  },
+
+  updateHash : function( direction ) {
+     // Update current Slides and hash.
+     location.hash = '#slide-' + Slides.currentSlide;
+  }
+};
+
+// All right; let's do this. 
+Slides.init(6);
